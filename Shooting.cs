fc9b3ff3e55e5f0459a3f823bac7db87e5266e8c
sean Y <sean.youngs@outlook.com>
2021-11-18 19:16:14 +0000

    Shooting mostly sorted. Create my own player sprite and recorded sounds for reloading.

--- Shooting.cs
+++ Shooting.cs
@@ -6,44 +6,116 @@ public class Shooting : MonoBehaviour
 {
 
     public GameObject rayCaster;
+    public GameObject ray;
     public float pistolDamage = 10f;
+    public int bullets = 7;
+
+    public AudioClip fire;
+    public AudioClip empty;
+    public AudioClip reloadSound;
+    public AudioClip reloadCockSound;
+
+
+    public bool canFire = true;
+
+    private AudioSource audioSource;
+    
 
     private Rigidbody2D rb;
     private RaycastHit hit;
-    // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+        canFire = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Shoot();
+        reload();
     }
     
     private void Shoot()
     {
+         if (Input.GetMouseButtonDown(0) && canFire == true)
+         {
 
-        if (Input.GetMouseButtonDown(0))
+            if (bullets > 0)
+            {
+
+                // ADD Sprite muzzle flash
+
+                // ADD RELOAD Sound
+
+                bullets--;
+
+                audioSource.PlayOneShot(fire);
+
+                RaycastHit2D hit = Physics2D.Raycast(ray.transform.position, rayCaster.transform.position);
+
+                EnemyDamage target = hit.transform.GetComponent<EnemyDamage>();
+
+                if (target != null)
+                {
+                    target.TakeDamage(pistolDamage);
+                }
+
+                if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider.name);
+                }
+
+
+
+                Debug.DrawRay(ray.transform.position, rayCaster.transform.position * 10, Color.blue, 5f);
+
+            }
+
+            else
+            {
+                audioSource.PlayOneShot(empty);
+            }
+
+         }
+    }
+
+
+    private void reload ()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, rayCaster.transform.position);
+            canFire = false;
 
-            EnemyDamage target = hit.transform.GetComponent<EnemyDamage>();
-
-            if (target != null)
+            if (bullets > 0)
             {
-                target.TakeDamage(pistolDamage);
+                audioSource.PlayOneShot(reloadSound);
+                StartCoroutine("ReloadCoroutine");
             }
 
-            if (hit.collider != null)
+            else
             {
-                Debug.Log(hit.collider.name);
+                audioSource.PlayOneShot(reloadCockSound);
+                StartCoroutine("FullReloadCoroutine");
             }
-
-            Debug.DrawRay(transform.position, rayCaster.transform.position * 10, Color.blue, 5f);
-            
         }
+    }
+
+    IEnumerator ReloadCoroutine()
+    {
+        yield return new WaitForSeconds(4f);
+        bullets = 8;
+        canFire = true;
+        yield return null;
+    }
+
+    IEnumerator FullReloadCoroutine()
+    {
+        yield return new WaitForSeconds(5f);
+        bullets = 7;
+        canFire = true;
+        yield return null;
     }
     
 }
